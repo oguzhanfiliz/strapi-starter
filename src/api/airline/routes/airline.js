fd1923b963eb726c9c77721a333079566a21727d
Oğuzhan Filiz <oguzhan.filiz@hititcs.com>
2023-09-13 23:22:06 +0300

    create airlines api

---
+++ src/api/airline/routes/airline.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * airline router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::airline.airline');
---
+++ src/api/airline/services/airline.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * airline service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::airline.airline');
